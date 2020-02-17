9a248109baae57a84e30dec6fe9fcecdb4f28ddd
Chih-Hui <53479045+huihuiman@users.noreply.github.com>
2020-02-17 11:46:31 +0800

    Update WebFrame.py

--- http_web/web/WebFrame.py
+++ http_web/web/WebFrame.py
@@ -16,10 +16,9 @@ class Application(object):
         self.sockfd.listen(5)
         while True:
             connfd,addr = self.sockfd.accept()
-
-            #接收请求方法
+            #接收方法
             method = connfd.recv(128).decode()
-            #接收请求内容
+            #接收内容
             path = connfd.recv(128).decode()
             print(method,path)
             if method == 'GET':
@@ -27,8 +26,7 @@ class Application(object):
                     status,response_body = self.get_html(path)           
                 else:
                     status,response_body = self.get_data(path)
-
-                #将结果给httpserver
+               
                 connfd.send(status.encode())
                 time.sleep(0.1)
                 connfd.send(response_body.encode()) 
@@ -51,16 +49,13 @@ class Application(object):
         finally:
             return response
 
-
     def get_data(self,path):
         for url,handler in urls: 
             if path == url:
                 response_body = handler()
                 return '200',response_body
-
         return '404','Sorry,Not found the data'
-
 
 if __name__ == "__main__":
     app = Application()
-    app.start() #启动框架等待request
+    app.start()
